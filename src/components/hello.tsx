2d4bef44818d824384fec9e088771d9e7de1ae9e
Shankar Venugoapl <shankarv82@gmail.com>
2019-05-14 12:21:14 +0530

    updated folder structure

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -11,6 +11,6 @@ export interface HelloState {
 
 export class Hello extends React.Component<HelloProps, HelloState> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Helloos from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
